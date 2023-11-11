b6eeac89dd642cd61d89fdfe7066c5272209b541
VladAkolzin <akolzin95@gmail.com>
2023-11-11 12:36:11 +0100

    task 1 + 2 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const list = document.querySelector('.gallery');
+list.insertAdjacentHTML('beforeend', createMarkUp(galleryItems));
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   				<a class="gallery__link" href="${original}">
+      			<img class="gallery__image" src="${preview}" alt="${description}" />
+   				</a>
+				</li>`
+    )
+    .join('');
+}
+let lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+const keyCurrentTime = 'videoplayer-current-time';
+const userVideoTime = localStorage.getItem(keyCurrentTime);
+if (userVideoTime) {
+  player.setCurrentTime(userVideoTime);
+}
+const findTime = function (data) {
+  const currentTime = data.seconds;
+  localStorage.setItem(keyCurrentTime, currentTime);
+};
+player.on('timeupdate', throttle(findTime, 1000));
